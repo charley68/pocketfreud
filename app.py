bd540601d754d66d856e9ae223595792f8942c06
Steve Lane <sclane68@yahoo.co.uk>
2025-04-09 10:18:33 +0100

    icon size

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def init_db():
         conn.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
+                username TEXT NOT NULL,
                 email TEXT UNIQUE NOT NULL,
                 password TEXT NOT NULL
             )
